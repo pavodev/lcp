557fba3b4ad3c1166f10c3fe67f0ee12d2a4cb21
Nikolina Rajovic <nikolina.rajovic@uzh.ch>
2023-06-12 11:09:40 +0200

    Hide DepRel graph when there is no data

--- frontend/src/components/results/DetailsModalView.vue
+++ frontend/src/components/results/DetailsModalView.vue
@@ -11,11 +11,13 @@
           role="tab"
           aria-controls="nav-dependency"
           aria-selected="true"
+          v-if="hasDepRel"
         >
           Dependency Visualizer
         </button>
         <button
           class="nav-link"
+          :class="hasDepRel ? '' : 'active'"
           id="nav-details-tab"
           data-bs-toggle="tab"
           data-bs-target="#nav-details"
@@ -34,11 +36,13 @@
         id="nav-dependency"
         role="tabpanel"
         aria-labelledby="nav-dependency-tab"
+        v-if="hasDepRel"
       >
         <DepRelView :data="data" :sentences="sentences" />
       </div>
       <div
         class="tab-pane fade"
+        :class="hasDepRel ? '' : 'active show'"
         id="nav-details"
         role="tabpanel"
         aria-labelledby="nav-details-tab"
@@ -63,6 +67,19 @@ import DetailsTableView from "@/components/results/DetailsTableView.vue";
 export default {
   name: "ResultsDetailsModalView",
   props: ["data", "sentences", "corpora"],
+  data() {
+    let deprel = false;
+    for (let i in this.sentences[1]){
+      let token = this.sentences[1][i]
+      if (token[4]) {
+        deprel = true
+        break;
+      }
+    }
+    return {
+      hasDepRel: deprel,
+    }
+  },
   components: {
     DepRelView,
     DetailsTableView,
