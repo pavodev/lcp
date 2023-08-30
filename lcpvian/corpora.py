5fd1826fd034bbd398b3f42d33f3871dce3a9284
Danny McDonald <daniel.mcdonald@uzh.ch>
2023-08-30 14:33:01 +0200

    add some exception handling for lama problems

--- lcpvian/corpora.py
+++ lcpvian/corpora.py
@@ -6,11 +6,13 @@ We use the complete version of this dict as app["config"], so no DB requests
 are needed for this request.
 """
 
+import logging
+
 from json.decoder import JSONDecodeError
 
-from aiohttp import web
-
 from .utils import _filter_corpora, _lama_user_details, ensure_authorised
+from aiohttp import web
+from aiohttp.client_exceptions import ClientOSError
 
 
 @ensure_authorised
@@ -26,7 +28,16 @@ async def corpora(request: web.Request) -> web.Response:
     is_vian = request_data.get("appType", "lcp") == "vian"
 
     if not request_data.get("all", False):
-        user_data = await _lama_user_details(request.headers)
+        try:
+            user_data = await _lama_user_details(request.headers)
+        except ClientOSError as err:
+            jso = {
+                "details": str(err),
+                "type": err.__class__.__name__,
+                "status": "warning",
+            }
+            logging.warning(f"Failed to login: {err}", extra=jso)
+            return web.json_response({"error": "no login possible", "status": 401})
         corpora = _filter_corpora(request.app["config"], is_vian, user_data)
     else:
         corpora = request.app["config"]
