9fd80835ef8aa8165974f07eb192103abe59cb04
Jeremy Zehr <jeremy.zehr@uzh.ch>
2023-10-12 18:50:10 +0200

    fix typo in update.sh and update abstract-query

--- parser/update.sh
+++ parser/update.sh
@@ -5,6 +5,6 @@ python lark_to_cobquec.py dqd_grammar.lark cobquec.auto.json
 lark-js dqd_grammar.lark -o dqd_parser.js
 
 echo "/* eslint-disable */" | cat - dqd_parser.js > temp_dqd_parser && mv temp_dqd_parser dqd_parser.js
-sed -i 's/\s.*"sws://localhost:9090/wstrict"/\/\/  "strict"/' dqd_parser.js
+sed -i 's/\s.*"strict"/\/\/  "strict"/' dqd_parser.js
 
 cp dqd_parser.js ../frontend/src/
