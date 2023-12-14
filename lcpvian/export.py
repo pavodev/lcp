c73056a50cfb089ca8383c899f61aaae4903fefa
Jeremy Zehr <jeremy.zehr@uzh.ch>
2023-12-14 15:59:02 +0100

    fix typing in export

--- lcpvian/export.py
+++ lcpvian/export.py
@@ -9,19 +9,18 @@ from .typed import JSONObject
 from .utils import ensure_authorised
 
 @ensure_authorised
-async def export(request: web.Request) -> web.Response:
+async def export(request: web.Request) -> web.StreamResponse:
     """
     Fetch arbitrary JSON data from redis
     """
     hashed: str = request.match_info["hashed"]
-    response: JSONObject
     
     job = Job.fetch(hashed, connection=request.app["redis"])
     
     finished_jobs = [Job.fetch(jid, connection=request.app["redis"]) for jid in request.app["query"].finished_job_registry.get_job_ids()]
     associated_jobs = [j for j in finished_jobs if j.kwargs.get("first_job") == hashed]
     
-    response = web.StreamResponse(
+    response: web.StreamResponse = web.StreamResponse(
         status=200,
         reason='OK',
         headers={'Content-Type': 'application/octet-stream', 'Content-Disposition': 'attachment; filename=results.txt'},
