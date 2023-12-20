e5ca9c902ae2cb0a0bda3e3f566c0a717ce7916b
Jeremy Zehr <jeremy.zehr@uzh.ch>
2023-12-20 17:30:33 +0100

    Implemented a first version of the Export button

--- lcpvian/export.py
+++ lcpvian/export.py
@@ -3,16 +3,77 @@
 from aiohttp import web
 from asyncio import sleep
 from rq.job import Job
+from typing import Any, cast
 from .utils import ensure_authorised
 
+import json
 
 CHUNKS = 1000000 # SIZE OF CHUNKS TO STREAM, IN # OF CHARACTERS
 
-async def stream_results(jobs: list[Job], resp: web.StreamResponse):
-    buffer: str = ""
-    for j in jobs:
-        for line in j.result:
+def _format_kwic(args: list, columns: list, sentences: dict[str,tuple], result_meta: dict) -> tuple[str,str,dict,list]:
+    kwic_name: str = result_meta.get("name","")
+    attributes: list = result_meta.get("attributes", [])
+    entities_attributes: dict = next((x for x in attributes if x.get("name","") == "entities"), dict({}))
+    entities: list = entities_attributes.get("data", [])
+    sid, matches = args
+    first_token_id, prep_seg = sentences[sid]
+    matching_entities: dict[str,list|int] = {n['name']: ([] if n.get("type") in ("sequence","set") else 0) for n in entities}
 
+    tokens = list()
+    for n, token in enumerate(prep_seg):
+        token_id = int(first_token_id) + n
+        for n_m, m in enumerate(matches):
+            if isinstance(m, int):
+                if m == token_id:
+                    matching_entities[entities[n_m]['name']] = token_id
+            elif isinstance(m, list):
+                if token_id in m:
+                    cast(list, matching_entities[entities[n_m]['name']]).append(token_id)
+        token_dict = {columns[n_col]: col for n_col, col in enumerate(token)}
+        token_dict["token_id"] = token_id
+        tokens.append(token_dict)
+
+    return (kwic_name,sid,matching_entities,tokens)
+
+
+async def kwic(jobs: list[Job], resp: web.StreamResponse, config):
+
+    sentence_jobs = [j for j in jobs if j.kwargs.get("sentences_query")]
+    other_jobs = [j for j in jobs if j not in sentence_jobs]
+
+    buffer: str = ""
+    for j in other_jobs:
+
+        corpus_index: str = str(j.kwargs.get("current_batch")[0])
+        segment_mapping = config[corpus_index]['mapping']['layer'][config[corpus_index]['segment']]
+        columns: list = []
+        if "partitions" in segment_mapping and 'languages' in j.kwargs:
+            lg = j.kwargs['languages'][0]
+            columns = segment_mapping['partitions'].get(lg)['prepared']['columnHeaders']
+        else:
+            columns = segment_mapping['prepared']['columnHeaders']
+
+        sentence_job: Job | None = next((sj for sj in sentence_jobs if sj.kwargs.get("depends_on") == j.id), None)
+        sentences: dict[str, tuple]
+        if sentence_job:
+            sentences = {str(uuid): (first_token_id, tokens) for (uuid, first_token_id, tokens) in sentence_job.result}
+        else:
+            continue
+
+        meta = j.kwargs.get("meta_json", {}).get("result_sets", [])
+        kwic_indices = [n+1 for n, o in enumerate(meta) if o.get("type") == "plain"]
+
+        for (n_type, args) in j.result:
+            # We're only handling kwic results here
+            if n_type not in kwic_indices:
+                continue
+            try:
+                kwic_name, sid, matching_entities, tokens = _format_kwic(args, columns, sentences, meta[n_type-1])
+                line: str = "\t".join([str(n_type),"plain",kwic_name,json.dumps({'sid': sid, 'matches': matching_entities, 'segment': tokens})])
+            except:
+                # Because queries for prepared segments only fetch what's needed for previewing purposes,
+                # queries with lots of matches can only output a small subset of lines
+                continue
             if len(f"{buffer}{line}\n") > CHUNKS:
                 await resp.write(buffer.encode("utf-8"))
                 await sleep(0.01) # Give the machine some time to breathe!
@@ -44,7 +105,25 @@ async def export(request: web.Request) -> web.StreamResponse:
     ]
     associated_jobs = [j for j in finished_jobs if j.kwargs.get("first_job") == hashed]
 
-    await stream_results([job, *associated_jobs], response)
+    meta = job.kwargs.get("meta_json", {}).get("result_sets", {})
+
+    await response.write((("\t".join(["index","type","label","data"])+f"\n")).encode("utf-8"))
+
+    # Write KWIC results
+    if next((m for m in meta if m.get("type")=="plain"),None):
+        await kwic([job, *associated_jobs], response, request.app["config"])
+
+    # Write non-KWIC results
+    for n, data in job.meta.get("all_non_kwic_results", {}).items():
+        if n in (0,-1):
+            continue
+        info = meta[n-1]
+        name = info.get("name","")
+        type = info.get("type","")
+        attr = info.get("attributes", [])
+        for line in data:
+            d: dict = {attr[n].get("name"): v for n, v in enumerate(line)}
+            await response.write(("\t".join([str(n),type,name,json.dumps(d)])+f"\n").encode("utf-8"))
     
     await response.write_eof()
     return response
