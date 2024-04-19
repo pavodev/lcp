9453c9f46a3a8d579ab69f259269436ea155b8ef
Danny McDonald <daniel.mcdonald@uzh.ch>
2024-04-19 12:18:12 +0200

    nearly there now, need to do lama bit now

--- lcpvian/action.py
+++ lcpvian/action.py
@@ -12,12 +12,16 @@ from time import sleep
 
 from .utils import load_env
 
-FE_INSTALL = "yarn install"
-FE_BUILD = "yarn build"
-FE_SERVE = "yarn serve"
-FE_YARN = "yarn"
+FE_INSTALL = "npm install"
+FE_BUILD_CP = "npm run build:catchphrase"
+FE_SERVE_CP = "npm run serve:catchphrase"
+FE_BUILD_CP = "npm run build:catchphrase"
+FE_SERVE_CP = "npm run serve:catchphrase"
+FE_BUILD_SS = "npm run build:soundscape"
+FE_SERVE_SS = "npm run serve:soundscape"
+FE_BUILD_SS = "npm run build:soundscape"
+FE_SERVE_SS = "npm run serve:soundscape"
 WORKER_START = "lcp-worker"
-NPM_INSTALL = "npm install"
 LCP = "lcp"
 
 load_env()
@@ -25,55 +29,64 @@ load_env()
 
 ROOT = os.path.dirname(os.path.dirname(__file__))
 FRONTEND_DIR = os.path.join(ROOT, "frontend")
-
-FRONTEND_SRC = os.path.join(FRONTEND_DIR, "src")
 EDROPS_DIR = os.path.join(ROOT, "vian-eventdrops")
 KILL_PORT = f"kill -9 `lsof -t -i:{os.getenv('AIO_PORT', '9090')}`"
 
 
 def npm_install():
-    return subprocess.Popen(NPM_INSTALL.split(), cwd=FRONTEND_SRC)
-
-
-def yarn():
-    return subprocess.Popen(FE_YARN.split(), cwd=FRONTEND_SRC)
-
-
-def build():
-    install().wait()
-    return subprocess.Popen(FE_BUILD.split(), cwd=FRONTEND_SRC)
+    return subprocess.Popen(FE_INSTALL.split(), cwd=FRONTEND_DIR)
 
 
 def edrop():
     return subprocess.Popen(FE_INSTALL.split(), cwd=EDROPS_DIR)
 
 
-def install():
-    return subprocess.Popen(FE_INSTALL.split(), cwd=FRONTEND_SRC)
+def build(ss: bool = False):
+    npm_install().wait()
+    cmd = FE_BUILD_SS if ss else FE_BUILD_CP
+    return subprocess.Popen(cmd.split(), cwd=FRONTEND_DIR)
 
 
-def serve():
-    return subprocess.Popen(FE_SERVE.split(), cwd=FRONTEND_SRC)
+def serve(ss: bool = False):
+    cmd = FE_SERVE_SS if ss else FE_SERVE_CP
+    return subprocess.Popen(cmd.split(), cwd=FRONTEND_DIR)
 
 
-def setup():
-    print("=============CALL SETUP================")
+def serve_ss():
+    return serve(ss=True)
+
+
+def serve_cp():
+    return serve(ss=False)
+
+
+def setup_cp():
     edrop().wait()
-    yarn().wait()
-    install().wait()
+    npm_install().wait()
     build().wait()
     print("Finished! You can now run `lcp-frontend-serve` to run the frontend...")
 
 
-def start_all() -> None:
+def setup_ss():
+    edrop().wait()
+    npm_install().wait()
+    build(ss=True).wait()
+    print("Finished! You can now run `lcp-frontend-serve` to run the frontend...")
+
+
+def start_app(ss: bool = False) -> None:
     """
     Todo: all of this but properly
     """
+    if ss:
+        server = serve_ss
+    else:
+        server = serve_cp
     try:
         subprocess.Popen(KILL_PORT, shell=True).wait()
     except:
         pass
-    commands = [serve()]
+    commands = [server()]
     sleep(15)
     commands.append(subprocess.Popen([WORKER_START], cwd=ROOT))
     sleep(15)
@@ -87,7 +100,7 @@ def start_all() -> None:
                     print(f"Commmand {c} gave {x}")
                     break
                 sleep(10)
-    except (Exception, KeyboardInterrupt) as err:
+    except BaseException as err:
         tb = traceback.format_exc()
         print(f"Error: {err}\n{tb}")
     finally:
@@ -105,3 +118,11 @@ def start_all() -> None:
             except:
                 pass
     return None
+
+
+def start_cp():
+    return start_app(ss=False)
+
+
+def start_ss():
+    return start_app(ss=True)
