a670042295f1355c8c32d61e410727817021937e
Jeremy Zehr <jeremy.zehr@uzh.ch>
2024-10-28 17:30:25 +0100

    Delete parser js files and update parser readme

--- parser/update.sh
+++ parser/update.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
 python lark_to_cobquec.py dqd_grammar.lark cobquec.auto.json
-
-lark-js dqd_grammar.lark -o dqd_parser.js
-
-echo "/* eslint-disable */" | cat - dqd_parser.js > temp_dqd_parser && mv temp_dqd_parser dqd_parser.js
-sed -i 's/\s.*"strict"/\/\/  "strict"/' dqd_parser.js
-sed -i 's/\s.*"ordered_sets"/\/\/  "ordered_sets"/' dqd_parser.js
-
-cp dqd_parser.js ../frontend/src/
